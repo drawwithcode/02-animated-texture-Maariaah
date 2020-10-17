c98027061a471a6bb208a55e06a40bd268605d95
Marie <42433966+Maariaah@users.noreply.github.com>
2020-10-17 21:39:06 +0200

    Update eyes

--- sketch.js
+++ sketch.js
@@ -3,24 +3,17 @@
 let eyesCollection = [];
 
 function setup() {
-  let colors = ["orange", "red", "blue"];
+  createCanvas(600, 600);
 
-  createCanvas(900, 900);
-  noStroke();
-  for (let i = 0; i < 10; i++) {
-    for (let c = 0; c < colors.length; c++) {
-      addEye(
-        Math.random() * 900,
-        Math.random() * 900,
-        Math.random() * 70,
-        colors[c]
-      );
-    }
+  for (let i = 0; i < 2; i++) {
+    addEye();
   }
 }
 
 function draw() {
-  background('#191D2D');
+  background("#191D2D");
+  stroke("#191D2D");
+  cursor("http://www.rw-designer.com/cursor-view/137065.png");
 
   for (let i = 0; i < eyesCollection.length; i++) {
     eyesCollection[i].display();
@@ -28,44 +21,65 @@ function draw() {
   }
 }
 
-function addEye(a, b, c, d) {
-  const newEye = new Eye(a, b, c, d);
-  eyesCollection.push(newEye);
+function addEye() {
+  let colors = ["orange", "red", "blue"];
+  for (var x = 0; x < width * 2; x += width / 5) {
+    for (var y = 0; y < height * 2; y += height / 5) {
+      const newEye = new Eye(x, y, random(colors));
+      eyesCollection.push(newEye);
+    }
+  }
 }
 
 class Eye {
-  constructor(tx, ty, ts, tc) {
+  constructor(tx, ty, tc) {
     this.x = tx;
     this.y = ty;
-    this.size = ts;
+    this.size = 40;
     this.angle = 30;
     this.color = tc;
   }
+
   update(mx, my) {
     this.angle = atan2(my - this.y, mx - this.x);
   }
+
+  openEye() {
+    ellipse(0, 0, this.size + 10, this.size + 10);
+    rotate(this.angle);
+    fill(this.color);
+    ellipse(0, 0, this.size, this.size);
+    fill(0);
+    ellipse(this.size / 4, 0, this.size / 2, this.size / 1.7);
+  }
+
+  moveEye() {}
+
+  closedEye() {
+    frameRate(1);
+    stroke("white");
+    line(-37, 0, -10 + 47, 0);
+  }
+
   display() {
     push();
-
+    fill(255, 255, 255, 70);
     translate(this.x, this.y);
-    fill(255, 255, 255, 127);
     beginShape();
-    vertex(-80, 0);
-    bezierVertex(-30, -50, 30, -50, 80, 0);
-    bezierVertex(30, 50, -30, 50, -80, 0);
+    vertex(-40, 0);
+    bezierVertex(-10, -40, 10, -40, 40, 0);
+    bezierVertex(10, 40, -10, 40, -40, 0);
     endShape();
 
-    if (random(20) < 2) {
-      frameRate(1);
-      stroke('white');
-      line(-80, 0, -30 + 110, 0);
+    if (dist(mouseX, mouseY, this.x, this.y) < this.size) {
+      if (mouseIsPressed) {
+        this.closedEye();
+      } else {
+        this.openEye();
+        this.moveEye();
+      }
     } else {
-      ellipse(0, 0, this.size + 10, this.size + 10);
-      rotate(this.angle);
-      fill(this.color);
-      ellipse(0, 0, this.size, this.size);
-      fill(0);
-      ellipse(this.size / 4, 0, this.size / 2, this.size / 1.7);
+      this.openEye();
     }
     pop();
   }
